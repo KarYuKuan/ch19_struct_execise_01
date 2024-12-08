e4b9f33a916f70abde682a5b64f51129d4a6e07f
Samurai_Chia <jyjzone@163.com>
2024-12-08 16:50:00 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::process::ExitStatus;
+
 fn main() {
-    println!("Hello, world!");
+
+    let u1 = User{
+        name:String::from("mike"),
+        user_id:123,
+        email:String::from("jyjzone@hotmail.com"),
+        score:89,
+    };
+}
+
+struct User {
+    name:String,
+    user_id:u8,
+    email:String,
+    score:u16,
 }
